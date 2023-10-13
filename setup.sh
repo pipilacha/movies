9925b0a337debc4649bcf81e397ddc4dff015e2a
Pipilacha <maucbs@gmail.com>
2023-10-12 18:09:46 -0600

    removed stuff from setup

--- setup.sh
+++ setup.sh
@@ -8,15 +8,9 @@ source ./venv/bin/activate && pip3 install -r requirements.txt
 echo ""
 
 echo ""
-echo "Copying Postgres service file to ~"
-cp -v .pg_service.conf ~
-echo "Remember to add password file. .moviesapp_db_pgpass  content: localhost:5432:moviesapp:moviesapp:password"
-echo ""
-
-echo ""
 echo "****************************************"
 echo " Dev Environment Setup Complete"
 echo "****************************************"
 echo ""
-echo "Use 'source ./venv/bin/activate' to switch this terminal to the venv"
+echo "Use 'source ./venv/bin/activate' to switch this terminal to the Virtual env"
 echo ""
